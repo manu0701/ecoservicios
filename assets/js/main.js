5d54ea9b200940a72cc3254b10896ef9e5bb078c
manu <manu.balquinto@gmail.com>
2020-05-29 11:11:08 -0300

    changes presup page

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,10 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
      document.querySelector('.nav-menu').classList.toggle('show')
 });
 
-// document.querySelector('body').addEventListener('click', () =>{
-//      document.querySelector('.nav-menu').classList.toggle('show')
-// });
+document.querySelector('ul.nav-menu').addEventListener('click', () => {
+     document.querySelector('.nav-menu').classList.toggle('show')
+})
 
-// $(document).on('click','.show',function(e) {
-//      if( $(e.target).is('a') ) {
-//          $(this).collapse('hide');
-//      }
-//  });
+// document.querySelector('ul.nav-menu').addEventListener('click', () => {
+//      document.querySelector('.nav-menu').classList.toggle('show')
+// })
