8ec147db36d15ef5e8d686d9d0d7fa039fe939d7
manu <manu.balquinto@gmail.com>
2020-05-28 21:57:40 -0300

    Add presupuesto page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+
 document.querySelector('.menu-btn').addEventListener('click', () => {
      document.querySelector('.nav-menu').classList.toggle('show')
 });
+
+// document.querySelector('body').addEventListener('click', () =>{
+//      document.querySelector('.nav-menu').classList.toggle('show')
+// });
+
+// $(document).on('click','.show',function(e) {
+//      if( $(e.target).is('a') ) {
+//          $(this).collapse('hide');
+//      }
+//  });
